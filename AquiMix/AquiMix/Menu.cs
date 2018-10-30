71185009fc4aa556266c69c659fe1d543cf09c6a
Oscar Garcia <oscarantoniox10@gmail.com>
2018-10-30 10:33:13 -0700

    Se agrego controles para el menu

--- AquiMix/AquiMix/Menu.cs
+++ AquiMix/AquiMix/Menu.cs
@@ -15,6 +15,47 @@ namespace AquiMix
         public Menu()
         {
             InitializeComponent();
+            Hotdoglbl.Text = Csencillo.ToString();
+            HDJumbo.Text = CJumbo.ToString();
+            HTDoblelbl.Text = CDoble.ToString();
+        }
+
+        public static int Csencillo = 0;
+        public static int CDoble = 0;
+        public static int CJumbo = 0;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Csencillo++;
+            Hotdoglbl.Text = Csencillo.ToString();
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Csencillo--;
+            Hotdoglbl.Text = Csencillo.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            CDoble++;
+            HTDoblelbl.Text = CDoble.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if(CDoble <= 0)
+            {
+                CDoble = 0;
+            }
+            else
+            {
+                CDoble--;
+                HTDoblelbl.Text = CDoble.ToString();
+            }
+            
         }
     }
 }
